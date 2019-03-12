2bc15f564feba5aa62adccd91296262b99cc5cdc
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2019-03-11 17:06:46 -0700

    add click event, ajax request in haunts.js

--- app/assets/javascripts/haunts.js
+++ app/assets/javascripts/haunts.js
@@ -1,4 +1,34 @@
 $(function () {
 	console.log('haunts.js is loaded')
+	listenForClick()
 });
 
+function listenForClick() {
+	$('button#haunts-data').on('click', function (event) {
+		event.preventDefault()
+		getHaunts()
+	});
+}
+
+function getHaunts() {
+	$.ajax({
+		url: 'http://localhost:3000/haunts',
+		method: 'get',
+		dataType: 'json'
+		}).done(function (data) {
+			console.log("The data is:", data)
+		}
+	)}
+
+
+	
+
+// class Haunt {
+// 	constructor(obj) {
+// 		this.id = obj.id
+// 		this.name = obj.name
+// 		this.city = obj.city
+// 		this.state = obj.state
+// 		this.description = obj.description
+// 	}
+// }
