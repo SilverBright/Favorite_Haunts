9ee390e64d65ce9a4bbdc646459aacc878029008
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2019-03-13 13:43:52 -0700

    add document.ready and test alert to haunts.js, update index page with a class to use with ajax

--- app/assets/javascripts/haunts.js
+++ app/assets/javascripts/haunts.js
@@ -1,22 +1,39 @@
+//the document (our index page) needs to be fully loaded before we can display our ajax results
+$(document).ready(function(){
+	$("a.ajax_load_haunts ").on("click", function(event) {
+	alert("You clicked this link")
+	event.preventDefault();
+	})
+})
+
+
+
+
+
+
 $(function () {
 	console.log('haunts.js is loaded')
 	listenForClick()
 });
 
+//hijack the click event for <button id='haunts-data'> on haunts.html.erb so you can insert an ajax request
 function listenForClick() {
 	$('button#haunts-data').on('click', function (event) {
+		//stop the default function of that click, which would cause the browser to reload
 		event.preventDefault()
+		//this is the name of the ajax request created below
 		getHaunts()
 	});
 }
 
+//This is the ajax request
 function getHaunts() {
 	$.ajax({
 		url: 'http://localhost:3000/haunts',
 		method: 'get',
 		dataType: 'json'
 		}).done(function (data) {
-			console.log("The data is:", data)
+			console.log("Here are all the Haunt JSON objects:", data)
 		}
 	)}
 
