763a90fa3688f1be4a256024239fb16de960862b
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2019-03-13 22:26:46 -0700

    start over on haunts.js, experiment with ajax requests

--- app/assets/javascripts/haunts.js
+++ app/assets/javascripts/haunts.js
@@ -1,16 +1,14 @@
-$(function(){
-	$("a.ajax_load_haunts ").on("click", function(event) {
-	event.preventDefault();
-		$.ajax({
-		method: 'get',
-		url: this.href,
-		dataType: 'json'
-	}).done(function (response) {
-		// $("div.haunts").html(response)
-		debugger
-
-		});
-	
+$(document).ready(function(){
+	$("a.ajax_load_haunts").on("click", function(event){
+		event.preventDefault();
 	})
 })
 
+$.ajax({
+	method: 'GET',
+	url: this.href,
+	dataType: 'json'
+}).done(function (response){
+	console.log("the response is:", response )
+	// $("div.haunts-json").append(response)
+})
