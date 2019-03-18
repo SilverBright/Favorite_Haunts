adf38d0263e3dcd68b4327bed669b5c52c9792db
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2019-03-18 14:13:44 -0700

    add constructor method for a haunt

--- app/assets/javascripts/haunts.js
+++ app/assets/javascripts/haunts.js
@@ -1,27 +1,34 @@
+//
 $(document).ready(function(){
-	console.log("haunts.js has loaded")
+	console.log("The document is loaded and ready. haunts.js has loaded")
 	listenForClick()
 })
 
+//Event listener for clicking on 'ajax_load_haunts'
 function listenForClick() {
 	$("a.ajax_load_haunts").on('click', function(event) {
+		// Stop the button from automatically clicking
 		event.preventDefault()
+		// Run the getHaunts ajax method below
 		getHaunts()
 	})
 }
 
+// Ajax method for getting Haunts and each of their comments
 function getHaunts() {
 	$.ajax({
 		url: 'http://localhost:3000/haunts',
 		method: 'get',
 		dataType: 'json'
 	}).done(function (response) {
-		console.log("Here is an array of Haunts:", response)
+		console.log("Here is the array of Haunts:", response)
 		
+		// All Haunts
 		response.forEach(function(data){
 			$("div.haunts").append(
 				"<ul>" + "<b>" + data.name + "</b>" + ", " + data.city + ", " + data.state + "<br>" + data.description +"<ul>");
 		
+		// All Comments for each Haunt
 		data.comments.forEach(function(comment){
 			$("div.haunts").append(
 				"<ul><b>A Reviewer Says: </b> " + "'" + comment.content + "'" + "</ul>" );
@@ -30,6 +37,20 @@ function getHaunts() {
 		});
 	event.preventDefault();
 	}
+
+// Create an instance of a Haunt (JavaScript Object Model)
+class Haunt {
+	constructor(object) {
+		this.id = object.id
+		this.name = object.name
+		this.description = object.description
+		this.city = object.city
+		this.state = object.state
+	}
+}
+
+
+
 
 
 
@@ -56,14 +77,3 @@ function getHaunts() {
 // 	event.preventDefault();
 // 	})
 // })
-
-// Create an instance of a Haunt (JavaScript Object Model)
-// class Haunt {
-// 	constructor(object) {
-// 		this.id = object.id
-// 		this.name = object.name
-// 		this.description = object.description
-// 		this.city = object.city
-// 		this.state = object.state
-// 	}
-// }
