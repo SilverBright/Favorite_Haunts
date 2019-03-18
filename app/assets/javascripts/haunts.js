6793cc61169b4499c8e95287fd4d97ae0d73b446
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2019-03-18 14:44:05 -0700

    add comments to haunts.js page

--- app/assets/javascripts/haunts.js
+++ app/assets/javascripts/haunts.js
@@ -1,10 +1,12 @@
-//
+//Read and load the entire document first
 $(document).ready(function(){
-	console.log("The document is loaded and ready. haunts.js has loaded")
+	console.log("The document is loaded and ready") 
+	console.log("The haunts.js file has loaded")
 	listenForClick()
 })
 
 //Event listener for clicking on 'ajax_load_haunts'
+// The ajax request is a 'promise' to return the data to you
 function listenForClick() {
 	$("a.ajax_load_haunts").on('click', function(event) {
 		// Stop the button from automatically clicking
@@ -14,28 +16,30 @@ function listenForClick() {
 	})
 }
 
-// Ajax method for getting Haunts and each of their comments
+// Ajax function for getting Haunts and Comments
 function getHaunts() {
 	$.ajax({
-		url: 'http://localhost:3000/haunts',
+		// url: 'http://localhost:3000/haunts',
+		url: this.href,
 		method: 'get',
 		dataType: 'json'
+		// When ajax is 'done' returning the data to you, run a function on it
 	}).done(function (response) {
 		console.log("Here is the array of Haunts:", response)
 		
-		// All Haunts
+		// Display all Haunts under "div.haunts" located on the haunts index page
 		response.forEach(function(data){
 			$("div.haunts").append(
 				"<ul>" + "<b>" + data.name + "</b>" + ", " + data.city + ", " + data.state + "<br>" + data.description +"<ul>");
 		
-		// All Comments for each Haunt
+		// Display all the nested Comments for Haunts under "div.haunts" located on the haunts index page
 		data.comments.forEach(function(comment){
 			$("div.haunts").append(
 				"<ul><b>A Reviewer Says: </b> " + "'" + comment.content + "'" + "</ul>" );
 				})
 			});
 		});
-	event.preventDefault();
+	// event.preventDefault();
 	}
 
 // Create an instance of a Haunt (JavaScript Object Model)
